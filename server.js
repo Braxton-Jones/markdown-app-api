929cc6409c676223669f7265969e1aaab5af5b14
Braxton <65457602+braxttsl-dev@users.noreply.github.com>
2023-03-22 13:46:43 -0400

    "deployment fix"

--- server.js
+++ server.js
@@ -2,18 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const documentRouter = require('./routes/documents');
-const cors = require('cors');
 const PORT = process.env.PORT;
 
 const api = express();
 
 // Parse incoming JSON data and implement route handler
-api.use(
-	cors({
-		origin: 'https://markdown-api.onrender.com/',
-		methods: ['GET', 'POST', 'DELETE', 'PATCH'],
-	}),
-);
 api.use(express.json());
 
 // Set CORS headers
